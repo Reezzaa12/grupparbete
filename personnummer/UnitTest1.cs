9a60b6f4df53e2b2ee524753d8db8e2ee48a8a09
Una Árnadóttir <unaemelia@gmail.com>
2024-01-15 14:46:44 +0100

    take out main code

--- personnummer/UnitTest1.cs
+++ personnummer/UnitTest1.cs
@@ -3,15 +3,8 @@ namespace personnummer;
 public class UnitTest1
 {
     [Fact]
-    public void TestValidPersonnummer()
+    public void Test1()
     {
-        // Arrange
-        string validPersonnummer = "000803-2872"; // Replace with a valid personnummer
 
-        // Act
-        bool isValid = Program.IsPersonnummerValid(validPersonnummer);
-
-        // Assert
-        Assert.True(isValid, "Valid personnummer should return true.");
     }
 }
