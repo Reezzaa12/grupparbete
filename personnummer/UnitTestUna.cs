c1c1764e3dd0f5ad3b1e3dbc10e0e5619fae64f7
Una Árnadóttir <unaemelia@gmail.com>
2024-01-16 10:13:45 +0100

    unit tests updated

--- personnummer/UnitTestUna.cs
+++ personnummer/UnitTestUna.cs
@@ -14,32 +14,4 @@ public class UnitTestUna
         // Then
         Assert.True(isValid, "Invalid personnummer should return false."); // output should be false
     }
-
-    [Fact]
-    public void TestGetGenderMale()
-    {
-        // Given
-        string malePersonnummer = "811218-9876";
-        string expectedGender = "Man";
-
-        // When
-        string actualGender = Program.GetGender(malePersonnummer);
-
-        // Then
-        Assert.Equal(expectedGender, actualGender);
-    }
-
-    [Fact]
-    public void TestGetGenderFemale()
-    {
-        // Given
-        string femalePersonnummer = "001219-3421";
-        string expectedGender = "Kvinna";
-
-        // When
-        string actualGender = Program.GetGender(femalePersonnummer);
-
-        // Then
-        Assert.Equal(expectedGender, actualGender);
-    }
 }
